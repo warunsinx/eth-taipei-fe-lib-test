00ed3d1f8e8377cb78b40519d12ddf8c907e8376
warunsinx <warunsinx@gmail.com>
2025-04-02 18:31:42 +0700

    feat: test more on 1inch port api

--- src/pages/api/oneinch.ts
+++ src/pages/api/oneinch.ts
@@ -1,8 +1,8 @@
-import { oneInchService } from "@/services/oneInchService";
-import { NextApiRequest, NextApiResponse } from "next";
+import { oneInchService } from '@/services/oneInchService';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const baseChainId = "8453";
-const usdTokenAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+const baseChainId = '8453';
+const usdTokenAddress = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913';
 
 export default async function handler(
   request: NextApiRequest,
@@ -10,25 +10,61 @@ export default async function handler(
 ) {
   try {
     const fetchBalance = await oneInchService.getWalletTokenBalances(
-      "0x52Aa899454998Be5b000Ad077a46Bbe360F4e497",
+      '0x52Aa899454998Be5b000Ad077a46Bbe360F4e497',
       [usdTokenAddress],
       baseChainId
     );
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const tokenPrices = await oneInchService.getTokenPrices(
       baseChainId,
       [usdTokenAddress],
-      "THB"
+      'THB'
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    const portfolioProfitAndLoss =
+      await oneInchService.getPortfolioProfitAndLoss(
+        ['0x52Aa899454998Be5b000Ad077a46Bbe360F4e497'],
+        baseChainId,
+        '1week'
+      );
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    const portfolioValueChart = await oneInchService.getPortfolioValueChart(
+      ['0x52Aa899454998Be5b000Ad077a46Bbe360F4e497'],
+      baseChainId,
+      '1week'
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    const portfolioCurrentValue = await oneInchService.getPortfolioCurrentValue(
+      ['0x52Aa899454998Be5b000Ad077a46Bbe360F4e497'],
+      baseChainId
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    const portfolioErc20Details = await oneInchService.getPortfolioErc20Details(
+      ['0x52Aa899454998Be5b000Ad077a46Bbe360F4e497'],
+      baseChainId,
+      '1week'
     );
 
     return response.status(200).send({
       fetchBalance,
       tokenPrices,
+      portfolioProfitAndLoss,
+      portfolioValueChart,
+      portfolioCurrentValue,
+      portfolioErc20Details,
     });
   } catch (error) {
-    console.error("Error fetching balance:", error);
+    console.error('Error fetching balance:', error);
     return response.status(500).send(error);
   }
 }
--- src/services/oneInchService.ts
+++ src/services/oneInchService.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { cfg } from "../configuration";
+import axios from 'axios';
+import { cfg } from '../configuration';
 
 const config = {
   headers: {
@@ -31,6 +31,96 @@ export const oneInchService = {
         currency,
       },
       config
+    );
+    return response.data;
+  },
+
+  getPortfolioProfitAndLoss: async (
+    walletAddress: string[],
+    chainId: string,
+    timeRange: string
+  ) => {
+    const response = await axios.get(
+      'https://api.1inch.dev/portfolio/portfolio/v4/general/profit_and_loss',
+      {
+        ...config,
+        params: {
+          chain_id: chainId,
+          timerange: timeRange,
+          addresses: walletAddress,
+        },
+        paramsSerializer: {
+          indexes: null,
+        },
+      }
+    );
+    return response.data;
+  },
+
+  getPortfolioValueChart: async (
+    walletAddress: string[],
+    chainId: string,
+    timeRange: string
+  ) => {
+    const response = await axios.get(
+      'https://api.1inch.dev/portfolio/portfolio/v4/general/value_chart',
+      {
+        ...config,
+        params: {
+          chain_id: chainId,
+          timerange: timeRange,
+          addresses: walletAddress,
+        },
+        paramsSerializer: {
+          indexes: null,
+        },
+      }
+    );
+    return response.data;
+  },
+
+  getPortfolioCurrentValue: async (
+    walletAddress: string[],
+    chainId: string
+  ) => {
+    const response = await axios.get(
+      'https://api.1inch.dev/portfolio/portfolio/v4/general/current_value',
+      {
+        ...config,
+        params: {
+          chain_id: chainId,
+          addresses: walletAddress,
+        },
+        paramsSerializer: {
+          indexes: null,
+        },
+      }
+    );
+    return response.data;
+  },
+
+  getPortfolioErc20Details: async (
+    walletAddress: string[],
+    chainId: string,
+    timeRange: string,
+    closed: boolean = true,
+    closedThreshold: number = 1
+  ) => {
+    const response = await axios.get(
+      'https://api.1inch.dev/portfolio/portfolio/v4/overview/erc20/details',
+      {
+        ...config,
+        params: {
+          chain_id: chainId,
+          addresses: walletAddress,
+          timerange: timeRange,
+          closed,
+          closed_threshold: closedThreshold,
+        },
+        paramsSerializer: {
+          indexes: null,
+        },
+      }
     );
     return response.data;
   },
